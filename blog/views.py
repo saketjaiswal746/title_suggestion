35cc7f1bba5b13c62557dfe9cb796966c7f3c83d
saketjaiswal746 <54267261+saketjaiswal746@users.noreply.github.com>
2024-09-27 12:36:23 +0530

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -11,10 +11,9 @@ def create_post(request):
         form = BlogPostForm(request.POST)
         if form.is_valid():
             content = form.cleaned_data['content']
-            # Generate title suggestions
             suggested_titles = suggest_titles(content)
             blog_post = BlogPost.objects.create(content=content, suggested_titles=suggested_titles)
-            return redirect('suggested_titles', pk=blog_post.pk)  # Redirect to view titles
+            return redirect('suggested_titles', pk=blog_post.pk)  
     else:
         form = BlogPostForm()
 
@@ -28,17 +27,14 @@ def suggested_titles(request, pk):
 def api_suggest_titles(request):
     if request.method == 'POST':
         try:
-            # Get content from the request body (assumed to be JSON)
             body = json.loads(request.body)
             content = body.get('content', '')
 
             if not content:
                 return JsonResponse({'error': 'No content provided'}, status=400)
 
-            # Generate title suggestions using the NLP model
             suggested_titles = suggest_titles(content)
 
-            # Return the suggestions as JSON
             return JsonResponse({'suggested_titles': suggested_titles}, status=200)
 
         except json.JSONDecodeError:
